c8db8ee98154fdbec36c27ab091924d720a29644
Mykyta Rusyn <nikrus1275@gmail.com>
2022-07-12 22:04:56 +0300

    changes in view/login

--- wwwroot/js/login.js
+++
@@ -1,6 +0,0 @@
-var passconf = document.getElementById(RegisterPasswordConfirm);
-console.log("1");
-passconf.onfocus = function () {
-	document.getElementById("match").style.display = "block";
-}
-console.log("2");
